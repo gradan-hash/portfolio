b381df892b1d71c980fda3768cce5d7dc19a03be
gradan-hash <corneliusmutuku55@gmail.com>
2023-02-13 15:59:07 +0300

    links

--- src/data.js
+++ src/data.js
@@ -10,26 +10,31 @@ export const products = [
   {
     id: 2,
     img: "https://images.pexels.com/photos/1212053/pexels-photo-1212053.jpeg?auto=compress&cs=tinysrgb&w=600",
-    link: "http://cornelius.com,",
+    link: "",
+    name: "coming soon",
   },
   {
     id: 3,
     img: "https://images.pexels.com/photos/5632379/pexels-photo-5632379.jpeg?auto=compress&cs=tinysrgb&w=600",
-    link: "http://cornelius.com,",
+    link: "",
+    name: "coming soon",
   },
   {
     id: 4,
     img: "https://images.pexels.com/photos/1481105/pexels-photo-1481105.jpeg?auto=compress&cs=tinysrgb&w=600",
-    link: "http://cornelius.com,",
+    link: "",
+    name: "coming soon",
   },
   {
     id: 5,
     img: "https://images.pexels.com/photos/7007172/pexels-photo-7007172.jpeg?auto=compress&cs=tinysrgb&w=600",
-    link: "http://cornelius.com,",
+    link: "",
+    name: "coming soon",
   },
   {
     id: 6,
     img: "https://images.pexels.com/photos/3034343/pexels-photo-3034343.jpeg?auto=compress&cs=tinysrgb&w=600",
-    link: "http://cornelius.com,",
+    link: "",
+    name: "coming soon",
   },
 ];
